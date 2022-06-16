79f797141f8d5717ebe7c87ea9810e57510b3026
nayakasandy <49088047+nayakasandy@users.noreply.github.com>
2022-06-16 10:12:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
+  ['6281368831544'],
+  ['6281368831544'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
