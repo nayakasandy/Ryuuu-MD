cede6380be83148940de4ad535ca0019dd6ea01c
nayakasandy <49088047+nayakasandy@users.noreply.github.com>
2022-06-16 13:18:08 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,6 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0813-6883-1544]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
