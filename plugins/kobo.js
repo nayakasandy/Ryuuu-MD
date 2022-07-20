1cbcf8fc0fc8bde9e1813219bc657ccf660baa6f
nayakasandy <49088047+nayakasandy@users.noreply.github.com>
2022-07-20 23:34:18 +0700

    Update kobo.js

--- plugins/kobo.js
+++ plugins/kobo.js
@@ -2,7 +2,7 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("kobo","kobo kanaeru","kobo hololive","kobo holoid","loli kobo","ecchi kobo").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+	scrap.pinterest("kobo kanaeru","kobo hololive","kobo vtuber","kobo kanaeru fan art","kobo kanaeru cute").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
    }
     
 handler.help = ['kobo']
